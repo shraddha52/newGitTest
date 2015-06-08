a3bd49ca6ae46c0fb90eb2f758c9c65184527373
shraddha <shraddha.waviz@gmail.com>
2015-06-08 13:58:29 +0530

    added src and bin

---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+
+public class Vehicle {
+	 
+ void car(){
+	 
+	 System.out.println("Car is a vehicle");
+ }
+  
+ public static void main(String args[]){
+	 
+	 
+	 Vehicle c=new Vehicle();
+	 c.car();
+	
+ }
+}
